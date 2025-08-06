83a94a0f7978ae3b99c99f1cf4a35701bb14ecac
dizzib <dizzib.dev@gmail.com>
2025-08-06 14:33:30 +0100

    add dragThreshold comment
    .

--- lib/src/overlay_window.dart
+++ lib/src/overlay_window.dart
@@ -36,6 +36,8 @@ class FlutterOverlayWindow {
   ///
   /// `enableDrag` to enable/disable dragging the overlay over the screen and default is "false"
   ///
+  /// `dragThreshold` to determine how easy it is to start dragging the overlay and default is 5 (device independant pixels)
+  ///
   /// `positionGravity` the overlay postion after drag and default is [PositionGravity.none]
   ///
   /// `startPosition` the overlay start position and default is null
